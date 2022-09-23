c3d12b0502f3d4d7f31240e1e6fc10dc9a64ff99
Lhwhatever <llinhong.2301+github@gmail.com>
2022-09-23 10:55:19 -0400

    Set up MUI and @emotion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import "../styles/globals.css";
+import * as React from "react";
 import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "../src/createEmotionCache";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/system";
+import theme from "../src/theme";
+import { CssBaseline } from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
+}
+
+function MyApp(props: MyAppProps) {
+    const {
+        Component,
+        emotionCache = clientSideEmotionCache,
+        pageProps,
+    } = props;
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1, width=device-width"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
 
 export default MyApp;
